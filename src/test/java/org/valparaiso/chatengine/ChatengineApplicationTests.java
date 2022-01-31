349c37ec4c7e825d425615d9f7a23901f8a9090c
dawood <damohammed@taxilla.com>
2022-02-01 05:17:40 +0530

    initial commit

---
+++ src/test/java/org/valparaiso/chatengine/ChatengineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.valparaiso.chatengine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatengineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
