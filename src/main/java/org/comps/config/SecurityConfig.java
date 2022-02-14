1377919ea7f9024a165420126c730a7a68db7728
Srinath <jagarlamudisrinath99@gmail.com>
2022-02-14 12:50:58 +0530

    enabled cors support

--- src/main/java/org/comps/config/SecurityConfig.java
+++ src/main/java/org/comps/config/SecurityConfig.java
@@ -2,9 +2,16 @@ package org.comps.config;
 
 import org.comps.auth.AppAuthSuccessHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
@@ -24,6 +31,17 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .successHandler(authSuccessHandler)
                 .and()
-                .logout().logoutSuccessUrl("/logout");
+                .logout().logoutSuccessUrl("/logout")
+                .and().cors(Customizer.withDefaults());
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST", "OPTIONS"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 }
